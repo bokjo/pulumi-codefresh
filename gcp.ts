9e9322322035d39a1c23a8003ff91fb0b38aeb2c
MitchellGerdisch <mitch@pulumi.com>
2020-12-29 10:44:37 -0600

    let codefresh fix gcp

--- gcp.ts
+++ gcp.ts
@@ -60,6 +60,11 @@ users:
 - name: ${context}
   user:
     auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
       name: gcp
 `;
   });
